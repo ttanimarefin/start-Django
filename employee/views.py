0a5d5a2411d6e843548e49bba08a7e353e62783a
ttanimarefin <ttanimarefin@gmail.com>
2021-02-28 12:18:19 +0600

    Apps links to main project

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+def employee(request):
+    return HttpResponse("This is Employee Page")
+
+def profile(request):
+    return HttpResponse("This is Employee Profile Page")
 
 # Create your views here.

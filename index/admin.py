d43f3ddd3c3ca27501a7e29d1962c32b9680482c
ttanimarefin <ttanimarefin@gmail.com>
2021-03-03 23:30:15 +0600

    migrate & migrations for about app

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import about
+admin.site.register(about)
 # Register your models here.
--- index/models.py
+++ index/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class aboutUs (models.Model):
+class about (models.Model):
     title=models.CharField(max_length=100,blank=False)
     description=models.TextField(max_length=700,blank=False)
     image=models.ImageField(upload_to='about/',blank=False)

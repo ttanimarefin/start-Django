488e27813debdc00b06faf7d67a69c5dfce76335
ttanimarefin <ttanimarefin@gmail.com>
2021-03-03 20:42:13 +0600

    create aboutus section models

--- index/models.py
+++ index/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class aboutUs (models.Model):
+    title=models.CharField(max_length=100,blank=False)
+    description=models.TextField(max_length=700,blank=False)
+    image=models.ImageField(upload_to='about/',blank=False)
+
+    
+
+    
